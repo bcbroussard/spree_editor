a5cb0079c59a70f6431475f6b12f786982b7492c
LiYang <theo@secondbureau.com>
2013-01-16 10:52:54 +0800

    fix bug precomplie

--- lib/spree_editor.rb
+++ lib/spree_editor.rb
@@ -4,6 +4,10 @@ module SpreeEditor
   class Engine < Rails::Engine
     config.autoload_paths += %W(#{config.root}/lib)
 
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w(admin/spree_editor.js editors/config.js)
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
